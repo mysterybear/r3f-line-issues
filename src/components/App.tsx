d54226013ddab7f006802ac18c83b22691fe949c
Tom Bear <tom@bearjam.dev>
2021-04-21 09:04:58 +0100

    ^^

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,24 @@
-import { Suspense } from "react"
-import CropImage from "./CropImage"
+import { useMemo } from "react"
+import * as THREE from "three"
 
 const App = () => {
+  const geom = useMemo(() => new THREE.PlaneBufferGeometry(), [])
   return (
-    <Suspense fallback={null}>
-      <CropImage />
-    </Suspense>
+    <group>
+      <mesh>
+        <primitive object={geom} attach="geometry" />
+        <meshBasicMaterial color="steelblue" side={2} />
+      </mesh>
+      <lineSegments>
+        <edgesGeometry args={[geom]} />
+        <lineBasicMaterial
+          color="red"
+          linewidth={5}
+          linecap="round"
+          linejoin="round"
+        />
+      </lineSegments>
+    </group>
   )
 }
 
